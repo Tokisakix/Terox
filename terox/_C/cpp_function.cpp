aff50ee8d52055dee88a2c48186ce9f316fe197c
TOKISAKIX\21168 <2116884726@qq.com>
2024-09-23 15:23:02 +0800

    add Cpp CUDA conv2d

--- terox/_C/cpp_function.cpp
+++ terox/_C/cpp_function.cpp
@@ -26,6 +26,121 @@ py::array_t<double> matmul(py::array_t<double> a, py::array_t<double> b) {
     return result;
 }
 
+py::array_t<double> conv2d_forward(py::array_t<double> a, py::array_t<double> w, int stride, int padding) {
+    auto a_buf = a.unchecked<4>();  // (batch_size, in_channels, height, width)
+    auto w_buf = w.unchecked<4>();  // (out_channels, in_channels, kernel_height, kernel_width)
+
+    ssize_t batch_size = a_buf.shape(0);
+    ssize_t in_channels = a_buf.shape(1);
+    ssize_t input_height = a_buf.shape(2);
+    ssize_t input_width = a_buf.shape(3);
+    ssize_t out_channels = w_buf.shape(0);
+    ssize_t kernel_height = w_buf.shape(2);
+    ssize_t kernel_width = w_buf.shape(3);
+
+    ssize_t output_height = (input_height + 2 * padding - kernel_height) / stride + 1;
+    ssize_t output_width = (input_width + 2 * padding - kernel_width) / stride + 1;
+
+    py::array_t<double> output({batch_size, out_channels, output_height, output_width});
+    auto output_buf = output.mutable_unchecked<4>();
+
+    std::vector<double> padded_size(batch_size * in_channels * (input_height + 2 * padding) * (input_width + 2 * padding), 0);
+    auto a_padded = padded_size.data();
+
+    for (ssize_t b = 0; b < batch_size; ++b) {
+        for (ssize_t c = 0; c < in_channels; ++c) {
+            for (ssize_t h = 0; h < input_height; ++h) {
+                for (ssize_t w = 0; w < input_width; ++w) {
+                    a_padded[b * in_channels * (input_height + 2 * padding) * (input_width + 2 * padding) +
+                              c * (input_height + 2 * padding) * (input_width + 2 * padding) +
+                              (h + padding) * (input_width + 2 * padding) +
+                              (w + padding)] = a_buf(b, c, h, w);
+                }
+            }
+        }
+    }
+
+    for (ssize_t i = 0; i < output_height; ++i) {
+        for (ssize_t j = 0; j < output_width; ++j) {
+            for (ssize_t oc = 0; oc < out_channels; ++oc) {
+                double sum = 0.0;
+                for (ssize_t ic = 0; ic < in_channels; ++ic) {
+                    for (ssize_t kh = 0; kh < kernel_height; ++kh) {
+                        for (ssize_t kw = 0; kw < kernel_width; ++kw) {
+                            sum += a_padded[(b * in_channels * (input_height + 2 * padding) * (input_width + 2 * padding) +
+                                             ic * (input_height + 2 * padding) * (input_width + 2 * padding) +
+                                             (i * stride + kh) * (input_width + 2 * padding) +
+                                             (j * stride + kw))] * w_buf(oc, ic, kh, kw);
+                        }
+                    }
+                }
+                output_buf(b, oc, i, j) = sum;
+            }
+        }
+    }
+
+    return output;
+}
+
+std::tuple<py::array_t<double>, py::array_t<double>> conv2d_backward(py::array_t<double> a, py::array_t<double> w, int stride, int padding, py::array_t<double> grad) {
+    auto a_buf = a.unchecked<4>();  // (batch_size, in_channels, height, width)
+    auto w_buf = w.unchecked<4>();  // (out_channels, in_channels, kernel_height, kernel_width)
+    auto grad_buf = grad.unchecked<4>();  // (batch_size, out_channels, grad_height, grad_width)
+
+    ssize_t batch_size = a_buf.shape(0);
+    ssize_t in_channels = a_buf.shape(1);
+    ssize_t input_height = a_buf.shape(2);
+    ssize_t input_width = a_buf.shape(3);
+    ssize_t out_channels = w_buf.shape(0);
+    ssize_t kernel_height = w_buf.shape(2);
+    ssize_t kernel_width = w_buf.shape(3);
+    ssize_t output_height = grad_buf.shape(2);
+    ssize_t output_width = grad_buf.shape(3);
+
+    std::vector<double> padded_size(batch_size * in_channels * (input_height + 2 * padding) * (input_width + 2 * padding), 0);
+    auto a_padded = padded_size.data();
+
+    for (ssize_t b = 0; b < batch_size; ++b) {
+        for (ssize_t c = 0; c < in_channels; ++c) {
+            for (ssize_t h = 0; h < input_height; ++h) {
+                for (ssize_t w = 0; w < input_width; ++w) {
+                    a_padded[b * in_channels * (input_height + 2 * padding) * (input_width + 2 * padding) +
+                              c * (input_height + 2 * padding) * (input_width + 2 * padding) +
+                              (h + padding) * (input_width + 2 * padding) +
+                              (w + padding)] = a_buf(b, c, h, w);
+                }
+            }
+        }
+    }
+
+    py::array_t<double> grad_a({batch_size, in_channels, input_height, input_width});
+    py::array_t<double> grad_w({out_channels, in_channels, kernel_height, kernel_width});
+    auto grad_a_buf = grad_a.mutable_unchecked<4>();
+    auto grad_w_buf = grad_w.mutable_unchecked<4>();
+
+    for (ssize_t i = 0; i < output_height; ++i) {
+        for (ssize_t j = 0; j < output_width; ++j) {
+            for (ssize_t oc = 0; oc < out_channels; ++oc) {
+                for (ssize_t ic = 0; ic < in_channels; ++ic) {
+                    for (ssize_t kh = 0; kh < kernel_height; ++kh) {
+                        for (ssize_t kw = 0; kw < kernel_width; ++kw) {
+                            grad_w_buf(oc, ic, kh, kw) += grad_buf(b, oc, i, j) * a_padded[(b * in_channels * (input_height + 2 * padding) * (input_width + 2 * padding) +
+                                                  ic * (input_height + 2 * padding) * (input_width + 2 * padding) +
+                                                  (i * stride + kh) * (input_width + 2 * padding) +
+                                                  (j * stride + kw))];
+                        }
+                    }
+                    grad_a_buf(b, ic, i * stride + kh, j * stride + kw) += grad_buf(b, oc, i, j) * w_buf(oc, ic, kh, kw);
+                }
+            }
+        }
+    }
+
+    return std::make_tuple(grad_a, grad_w);
+}
+
 PYBIND11_MODULE(cpp_function, m) {
-    m.def("matmul", matmul, "C tensor.matmul()");
+    m.def("matmul", &matmul, "C tensor.matmul()");
+    m.def("conv2d_forward", &conv2d_forward, "C tensor.conv2d()");
+    m.def("conv2d_backward", &conv2d_backward, "C tensor.conv2d()");
 }
--- terox/_C/cuda_function.cu
+++ terox/_C/cuda_function.cu
@@ -50,6 +50,197 @@ py::array_t<double> matmul_cuda(py::array_t<double> a, py::array_t<double> b) {
     return result;
 }
 
+__global__ void conv2d_forward_kernel(const double* a, const double* w, double* output, 
+                                       int batch_size, int in_channels, int input_height, int input_width,
+                                       int out_channels, int kernel_height, int kernel_width,
+                                       int output_height, int output_width, int stride, int padding) {
+    int batch = blockIdx.x;
+    int oc = blockIdx.y;
+    int i = blockIdx.z / output_width;
+    int j = blockIdx.z % output_width;
+
+    if (batch < batch_size && oc < out_channels && i < output_height && j < output_width) {
+        double sum = 0.0;
+        for (int ic = 0; ic < in_channels; ++ic) {
+            for (int kh = 0; kh < kernel_height; ++kh) {
+                for (int kw = 0; kw < kernel_width; ++kw) {
+                    int h = i * stride + kh;
+                    int w = j * stride + kw;
+                    if (h < input_height + 2 * padding && w < input_width + 2 * padding) {
+                        sum += a[batch * in_channels * (input_height + 2 * padding) * (input_width + 2 * padding) +
+                                  ic * (input_height + 2 * padding) * (input_width + 2 * padding) +
+                                  (h + padding) * (input_width + 2 * padding) +
+                                  (w + padding)] * 
+                                w[oc * in_channels * kernel_height * kernel_width + 
+                                  ic * kernel_height * kernel_width + 
+                                  kh * kernel_width + kw];
+                    }
+                }
+            }
+        }
+        output[batch * out_channels * output_height * output_width + 
+               oc * output_height * output_width + 
+               i * output_width + j] = sum;
+    }
+}
+
+py::array_t<double> conv2d_forward(py::array_t<double> a, py::array_t<double> w, int stride, int padding) {
+    auto a_buf = a.unchecked<4>();
+    auto w_buf = w.unchecked<4>();
+
+    ssize_t batch_size = a_buf.shape(0);
+    ssize_t in_channels = a_buf.shape(1);
+    ssize_t input_height = a_buf.shape(2);
+    ssize_t input_width = a_buf.shape(3);
+    ssize_t out_channels = w_buf.shape(0);
+    ssize_t kernel_height = w_buf.shape(2);
+    ssize_t kernel_width = w_buf.shape(3);
+
+    ssize_t output_height = (input_height + 2 * padding - kernel_height) / stride + 1;
+    ssize_t output_width = (input_width + 2 * padding - kernel_width) / stride + 1;
+
+    py::array_t<double> output({batch_size, out_channels, output_height, output_width});
+    double* d_a;
+    double* d_w;
+    double* d_output;
+
+    cudaMalloc(&d_a, batch_size * in_channels * (input_height + 2 * padding) * (input_width + 2 * padding) * sizeof(double));
+    cudaMalloc(&d_w, out_channels * in_channels * kernel_height * kernel_width * sizeof(double));
+    cudaMalloc(&d_output, batch_size * out_channels * output_height * output_width * sizeof(double));
+
+    cudaMemcpy(d_a, a.data(), batch_size * in_channels * input_height * input_width * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_w, w.data(), out_channels * in_channels * kernel_height * kernel_width * sizeof(double), cudaMemcpyHostToDevice);
+
+    dim3 blocks(batch_size, out_channels, output_height * output_width);
+    dim3 threads(1, 1, 1);
+
+    conv2d_forward_kernel<<<blocks, threads>>>(d_a, d_w, d_output, batch_size, in_channels, input_height, input_width,
+                                               out_channels, kernel_height, kernel_width, 
+                                               output_height, output_width, stride, padding);
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(output.mutable_data(), d_output, batch_size * out_channels * output_height * output_width * sizeof(double), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_a);
+    cudaFree(d_w);
+    cudaFree(d_output);
+
+    return output;
+}
+
+__global__ void conv2d_backward_kernel(const double* a, const double* w, const double* grad,
+                                        double* grad_a, double* grad_w,
+                                        int batch_size, int in_channels, int input_height, int input_width,
+                                        int out_channels, int kernel_height, int kernel_width,
+                                        int output_height, int output_width, int stride, int padding) {
+    int batch = blockIdx.x;
+    int oc = blockIdx.y;
+    int i = blockIdx.z / output_width;
+    int j = blockIdx.z % output_width;
+
+    if (batch < batch_size && oc < out_channels && i < output_height && j < output_width) {
+        for (int ic = 0; ic < in_channels; ++ic) {
+            for (int kh = 0; kh < kernel_height; ++kh) {
+                for (int kw = 0; kw < kernel_width; ++kw) {
+                    int h = i * stride + kh;
+                    int w = j * stride + kw;
+                    if (h < input_height + 2 * padding && w < input_width + 2 * padding) {
+                        // 计算权重梯度
+                        atomicAdd(&grad_w[oc * in_channels * kernel_height * kernel_width + 
+                                          ic * kernel_height * kernel_width + 
+                                          kh * kernel_width + kw],
+                                   grad[batch * out_channels * output_height * output_width + 
+                                        oc * output_height * output_width + 
+                                        i * output_width + j] *
+                                   a[batch * in_channels * (input_height + 2 * padding) * (input_width + 2 * padding) +
+                                     ic * (input_height + 2 * padding) * (input_width + 2 * padding) +
+                                     (h + padding) * (input_width + 2 * padding) +
+                                     (w + padding)]);
+                    }
+                }
+            }
+        }
+
+        // 计算输入梯度
+        for (int ic = 0; ic < in_channels; ++ic) {
+            for (int kh = 0; kh < kernel_height; ++kh) {
+                for (int kw = 0; kw < kernel_width; ++kw) {
+                    int h = i * stride + kh;
+                    int w = j * stride + kw;
+                    if (h < input_height + 2 * padding && w < input_width + 2 * padding) {
+                        atomicAdd(&grad_a[batch * in_channels * (input_height + 2 * padding) * (input_width + 2 * padding) +
+                                          ic * (input_height + 2 * padding) * (input_width + 2 * padding) +
+                                          (h + padding) * (input_width + 2 * padding) +
+                                          (w + padding)],
+                                       grad[batch * out_channels * output_height * output_width + 
+                                            oc * output_height * output_width + 
+                                            i * output_width + j] * 
+                                       w[oc * in_channels * kernel_height * kernel_width +
+                                         ic * kernel_height * kernel_width +
+                                         kh * kernel_width + kw]);
+                    }
+                }
+            }
+        }
+    }
+}
+
+std::tuple<py::array_t<double>, py::array_t<double>> conv2d_backward(py::array_t<double> a, py::array_t<double> w, int stride, int padding, py::array_t<double> grad) {
+    auto a_buf = a.unchecked<4>();
+    auto w_buf = w.unchecked<4>();
+    auto grad_buf = grad.unchecked<4>();
+
+    ssize_t batch_size = a_buf.shape(0);
+    ssize_t in_channels = a_buf.shape(1);
+    ssize_t input_height = a_buf.shape(2);
+    ssize_t input_width = a_buf.shape(3);
+    ssize_t out_channels = w_buf.shape(0);
+    ssize_t kernel_height = w_buf.shape(2);
+    ssize_t kernel_width = w_buf.shape(3);
+    ssize_t output_height = grad_buf.shape(2);
+    ssize_t output_width = grad_buf.shape(3);
+
+    py::array_t<double> grad_a({batch_size, in_channels, input_height, input_width});
+    py::array_t<double> grad_w({out_channels, in_channels, kernel_height, kernel_width});
+    double* d_a;
+    double* d_w;
+    double* d_grad;
+    double* d_grad_a;
+    double* d_grad_w;
+
+    cudaMalloc(&d_a, batch_size * in_channels * (input_height + 2 * padding) * (input_width + 2 * padding) * sizeof(double));
+    cudaMalloc(&d_w, out_channels * in_channels * kernel_height * kernel_width * sizeof(double));
+    cudaMalloc(&d_grad, batch_size * out_channels * output_height * output_width * sizeof(double));
+    cudaMalloc(&d_grad_a, batch_size * in_channels * (input_height + 2 * padding) * (input_width + 2 * padding) * sizeof(double));
+    cudaMalloc(&d_grad_w, out_channels * in_channels * kernel_height * kernel_width * sizeof(double));
+
+    cudaMemcpy(d_a, a.data(), batch_size * in_channels * input_height * input_width * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_w, w.data(), out_channels * in_channels * kernel_height * kernel_width * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_grad, grad.data(), batch_size * out_channels * output_height * output_width * sizeof(double), cudaMemcpyHostToDevice);
+
+    dim3 blocks(batch_size, out_channels, output_height * output_width);
+    dim3 threads(1, 1, 1);
+
+    conv2d_backward_kernel<<<blocks, threads>>>(d_a, d_w, d_grad, d_grad_a, d_grad_w, 
+                                                 batch_size, in_channels, input_height, input_width,
+                                                 out_channels, kernel_height, kernel_width,
+                                                 output_height, output_width, stride, padding);
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(grad_a.mutable_data(), d_grad_a, batch_size * in_channels * (input_height + 2 * padding) * (input_width + 2 * padding) * sizeof(double), cudaMemcpyDeviceToHost);
+    cudaMemcpy(grad_w.mutable_data(), d_grad_w, out_channels * in_channels * kernel_height * kernel_width * sizeof(double), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_a);
+    cudaFree(d_w);
+    cudaFree(d_grad);
+    cudaFree(d_grad_a);
+    cudaFree(d_grad_w);
+
+    return std::make_tuple(grad_a, grad_w);
+}
+
 PYBIND11_MODULE(cuda_function, m) {
-    m.def("matmul", matmul_cuda, "CUDA tensor.matmul()");
+    m.def("matmul", &matmul_cuda, "CUDA tensor.matmul()");
+    m.def("conv2d_forward", &conv2d_forward, "CUDA tensor.conv2d()");
+    m.def("conv2d_backward", &conv2d_backward, "CUDA tensor.conv2d()");
 }
